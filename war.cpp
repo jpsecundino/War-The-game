183b5eea86a94ba2349b7e94a5509134d69326da
jpsecundino <jp.secundino@usp.br>
2018-08-18 20:46:46 -0300

    primeira atualização

---
+++ war.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+
+struct Territorio{
+	int id_territorio;
+	int qtd_de_soldados;
+	int id_dono;
+
+}
+
+struct Objetivo{
+	int id_objetivo;
+	string descricao_objetivo;
+};
+
+struct Jogador{
+	int id;
+	string nome;
+	obj objetivo;
+	vector<int> cartas;
+};
+
