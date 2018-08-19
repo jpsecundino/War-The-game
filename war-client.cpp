aa7d0caacffea218f7dcb569cb9548e0b4024c8c
jpsecundino <jp.secundino@usp.br>
2018-08-18 22:44:54 -0300

    atualização do Makefile e criação de uma janela no arquivo "war-client"

--- war-client.cpp
+++ war-client.cpp
@@ -1,6 +1,23 @@
 #include "war.h"
+#include <SFML/Window.hpp>
 
-int main() {
-	return 0;
+
+int main()
+{
+    sf::Window window(sf::VideoMode(1920, 1080), "My window");
+    sf::Style::Close;
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    return 0;
 }
-
