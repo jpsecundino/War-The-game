c235b937a060dc9399f60937169847513834daac
Bráulio Bezerra <brauliobz@usp.br>
2018-08-18 22:38:34 -0300

    Rascunho do servidor

--- war-server.cpp
+++ war-server.cpp
@@ -1,5 +1,21 @@
 #include "war.h"
 
 int main() {
+
+	// TODO register and listen to sockets
+	// TODO accept clients
+	// TODO setup and start game
+	// TODO loop rounds
+		// TODO anounce new round
+		// TODO for each player (in sorted order)
+			// TODO receive cards
+			// TODO calc how many new soldiers
+			// TODO receive player distribution of soldiers (and check objective)
+			// TODO receive player attacks (and check objective)
+			// TODO receive player movements (and objective)
+			// TODO give card to player
+	// TODO announce winner
+	// TODO close sockets
+
 	return 0;
 }
