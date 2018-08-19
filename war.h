b3a7a75c6c4b907bf659c7e71932baf36d4d8873
jpsecundino <jp.secundino@usp.br>
2018-08-18 21:44:36 -0300

    atualização para incluir cartas especiais

---
+++ war.h
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+ 
+enum GeomFig{
+	Circunf,
+	Square,
+	Triang
+};
+
+enum Color{
+	white,
+	black,
+	red,
+	blue,
+	yellow,
+	green
+};
+
+struct Territory{
+	int territory_id;
+	int territory_soldiers_count;
+	int territory_owner_id;
+
+};
+
+struct Objective{
+	int objective_id;
+	string objective_descript;
+};
+
+struct Cards{
+	int card_id;
+	int card_territory_id;
+	GeomFig card_figure;
+	bool card_wildcard; 
+
+};
+
+struct Player{
+	int id;
+	string player_name;
+	obj player_objective;
+	vector<Cards> player_cards;
+	Color player_color;
+	string player_ip;
+	int player_exchanges; 
+};
+
+
+
