19e521bb0fe18d5b88fe75e3e9c48f879e4c8c4f
jpsecundino <jp.secundino@usp.br>
2018-08-18 21:12:30 -0300

    verificação das structs- Falta algo?

--- war.cpp
+++ war.cpp
@@ -1,29 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string>
-
-struct Territory{
-	int id_territory;
-	int soldiers_count;
-	int id_owner;
-
-};
-
-struct Objective{
-	int id_objective;
-	string obj_descript;
-};
  
 enum GeomFig{
 	Circunf,
 	Square,
-	Triang,
+	Triang
 };
+
+enum Color{
+	white,
+	black,
+	red,
+	blue,
+	yellow,
+	green
+};
+
+struct Territory{
+	int territory_id;
+	int territory_soldiers_count;
+	int territory_owner_id;
+
+};
+
+struct Objective{
+	int objective_id;
+	string objective_descript;
+};
+
+struct Cards{
+	int card_id;
+	int card_territory_id;
+	GeomFig card_figure; 
+
+};
+
 
 struct Player{
 	int id;
 	string player_name;
-	obj objective;
-	vector<int> cards;
+	obj player_objective;
+	vector<Cards> player_cards;
+	Color player_color;
+	string player_ip;
 };
+
 
